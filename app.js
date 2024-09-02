33af438b90da763da2823eba06c8529bef53d595
Pharm4U <153307106+Pharm4U@users.noreply.github.com>
2024-09-02 21:23:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import nodemailer from "nodemailer"
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
